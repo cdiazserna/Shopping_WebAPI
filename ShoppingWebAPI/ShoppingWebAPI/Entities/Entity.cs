fe3a0f325283bf8f78338a607fee7f38054d3f10
Carlos Diaz <junior.diaz@selazar.com>
2023-03-15 07:04:31 -0500

    New WebAPI Shopping project with new Country Entity

---
+++ ShoppingWebAPI/ShoppingWebAPI/Entities/Entity.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShoppingWebAPI.Entities
+{
+    public class Entity
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public DateTime? ModifiedDate { get; set; }
+    }
+}
