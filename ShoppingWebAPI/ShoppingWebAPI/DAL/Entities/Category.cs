48c17185800a5f87e7db5555d2e96dbf1dcf8cff
ASUS <ASUS@LAPTOP-10NQ3S9K>
2023-05-10 06:51:22 -0500

    New Project WebPages to consume the ShoppingWebAPI Index action created with its HTML

--- ShoppingWebAPI/ShoppingWebAPI/DAL/Entities/Category.cs
+++ ShoppingWebAPI/ShoppingWebAPI/DAL/Entities/Category.cs
@@ -4,6 +4,7 @@ namespace ShoppingWebAPI.DAL.Entities
 {
     public class Category : Entity
     {
+        //MODELO ESPEJO
         [Display(Name = "Categoría")]
         [MaxLength(100, ErrorMessage = "El campo {0} debe tener máximo {1} caracteres.")]
         [Required(ErrorMessage = "El campo {0} es obligatorio.")]
